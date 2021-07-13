a934f5615bb2f40800092084360a4b7e6ada10bd
boingboing <linayachika@gmail.com>
2021-07-13 15:16:07 +0900

    Update email_test.py

--- email_test.py
+++ email_test.py
@@ -1,24 +1,30 @@
 # -*- coding:utf-8 -*-
 
-import smtplib
+import smtplib # 파이썬은 자체적으로 SMTP 모듈을 지원한다.
 
 from email.mime.text import MIMEText
 
 
 smtp = smtplib.SMTP('smtp.gmail.com', 587) # 지메일 포트 -> 587 
 
-smtp.ehlo()
+smtp.ehlo() # say Hello 
 
-smtp.starttls()
+smtp.starttls() # TLS  보안 시작 
 
-smtp.login('linayachika@outlook.com', 'cho@2013')
+smtp.login('cnkegnome@gmail.com', '8804822!!')  # 로그인 인증
 
-msg = MIMEText('본문 테스트')
+# 보낼 메세지 설정 
 
-msg['Subject'] = '테스트'
+msg = MIMEText('JOB ID : 본문 테스트')
 
-msg['To'] = 'kim@naver.com'
+msg['Subject'] = '제목 테스트'
 
-smtp.sendmail('linayachika@outlook.com', 'linayachika2@gmail.com', msg.as_string())
+msg['To'] = 'sandartchippp@naver.com'
+
+
+smtp.sendmail('cnkegnome@gmail.com', msg['To'] , msg.as_string())
+
+#smtp.sendmail(   sender,   receiver , 메세지 내용 )
 
 smtp.quit() 
+
